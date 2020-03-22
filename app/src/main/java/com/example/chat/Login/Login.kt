2c7d987074aa5290c0070bcbcfff9e5589e30ff1
Ratmir <zxcasd123hdhs@gmail.com>
2020-03-22 21:01:25 +0300

    design

--- app/src/main/java/com/example/chat/Login/Login.kt
+++ app/src/main/java/com/example/chat/Login/Login.kt
@@ -2,9 +2,11 @@ package com.example.chat.Login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.example.chat.R
 
 
@@ -12,14 +14,30 @@ class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-            val btn: Button = findViewById(R.id.material_text_button)
+            val btn: Button = this.findViewById(R.id.material_text_button)
             val edt: EditText = findViewById(R.id.tel)
         btn.setOnClickListener {
             val telsize = edt.text
             if (telsize.length == 10){
-                val dil = DialogTest(edt.text.toString(), this)
-                val fm = supportFragmentManager
-                dil.show(fm, "hui")}
+                val builder2 = AlertDialog.Builder(this)
+                val builder = AlertDialog.Builder(this)
+                val mDialogView = LayoutInflater.from(this).inflate(R.layout.alertdialog, null)
+                builder.setTitle("+7 ")
+                builder.setMessage("На указанный номер придет SMS с кодом активации")
+                //builder.setPositiveButton("OK", DialogInterface.OnClickListener(function = x))
+
+                builder.setPositiveButton("Далее") { dialog, which ->
+                    Toast.makeText(applicationContext,
+                        android.R.string.yes, Toast.LENGTH_SHORT).show()
+                    builder2.show()
+                }
+
+                builder.setNegativeButton("Изменить") { dialog, which ->
+                    Toast.makeText(applicationContext,
+                        android.R.string.no, Toast.LENGTH_SHORT).show()
+                }
+                builder.show()
+                builder2.setView(mDialogView)}
             else{
                 Toast.makeText(
                     this, "Некорректный номер телефона",
