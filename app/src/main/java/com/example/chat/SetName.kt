5d1d0d4c1d7fbfa5b748ab066302e7d7df9ad150
Ratmir <zxcasd123hdhs@gmail.com>
2020-03-24 16:46:29 +0300

    design

--- app/src/main/java/com/example/chat/SetName.kt
+++ app/src/main/java/com/example/chat/SetName.kt
@@ -2,11 +2,15 @@ package com.example.chat
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputLayout
 
 class SetName: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setname)
+        val userName: TextInputLayout = findViewById(R.id.userName)
+        val btnNext: MaterialButton = findViewById(R.id.material_next_button)
     }
 
 }
